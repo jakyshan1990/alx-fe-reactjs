5577b6077a66288c6f1de1c8a4ac4c579294d61b
jakyshan1990 <jakyshan1990@yahoo.com>
2025-01-18 23:18:17 +0200

    commi2

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile(props){
     return(
 <div>
 <h2>{props.name}</h2>
-<p>AGE: {props.age}</p>
-<p>BIO: {props.bio}</p>
+<p>Age: {props.age}</p>
+<p>Bio: {props.bio}</p>
 
 
 </div>
